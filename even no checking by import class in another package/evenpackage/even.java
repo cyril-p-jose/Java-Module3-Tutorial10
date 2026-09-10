e060b85ba9adbf4c4d5482e28e2af044d30525c6
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-09-10 18:55:30 +0530

    Add even class with method to check even numbers

---
+++ even no checking by import class in another package/evenpackage/even.java
@@ -0,0 +1,14 @@
+package evenpackage;
+
+
+public class even {
+
+    public static boolean checkEven(int number) {
+
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
